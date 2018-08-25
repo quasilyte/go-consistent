d6a4b3f7626c7022ee1a99f6dad995836e13679f
Iskander Sharipov <quasilyte@gmail.com>
2018-08-26 02:07:09 +0300

    add internal/end2end package

---
+++ internal/end2end/end2end.go
@@ -0,0 +1,92 @@
+package end2end
+
+import (
+	"fmt"
+	"go/token"
+	"io"
+	"io/ioutil"
+	"regexp"
+)
+
+// ParseTestFile parses file at specified path using the parser with default settings.
+func ParseTestFile(filename string) (*TestFile, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("read test file: %v", err)
+	}
+	var p TestParser
+	return p.parseFile(filename, data)
+}
+
+// TestFile represents parsed end2end test file.
+type TestFile struct {
+	// Matchers is a mapping from source code line to the
+	// list of matchers for it.
+	Matchers map[int][]*Matcher
+
+	text string
+}
+
+// Text returns source code file contents that were used to create f.
+func (f *TestFile) Text() string { return f.text }
+
+// Matcher is a single parsed magic comment that is used
+// to match tested program output.
+type Matcher struct {
+	// Matches is a counter of matches for this matcher.
+	// Expected to be set by the user.
+	Matches int
+
+	text string
+	pos  token.Position
+	re   *regexp.Regexp // nil for "literal" (non-regexp) matchers
+}
+
+// MatchWithLine does text+position matching.
+//
+// Like Match, but also checks that m.Position().Line is
+// equal to the provided line argument.
+func (m *Matcher) MatchWithLine(s string, line int) bool {
+	return m.Match(s) && m.pos.Line == line
+}
+
+// Match tries to match s string against matcher pattern.
+func (m *Matcher) Match(s string) bool {
+	if m.re != nil {
+		return m.re.MatchString(s)
+	}
+	return m.text == s
+}
+
+// IsMatched reports whether m is matched at least once.
+func (m *Matcher) IsMatched() bool { return m.Matches != 0 }
+
+// Text returns the matcher comment text.
+func (m *Matcher) Text() string { return m.text }
+
+// Position returns matcher comment text position.
+func (m *Matcher) Position() token.Position { return m.pos }
+
+// TestParser is a end2end source file parser.
+type TestParser struct {
+	// MatcherRE is used to distinguish comments that describe matchers
+	// and also to capture their text.
+	//
+	// Regexp must include 2 capture groups:
+	//	1. Matches [=~]. This determines the matcher kind (text or regexp)
+	//	2. Matches text being matched. Usually something like ".*"
+	//
+	// If nil, defaultMatcherRE is used. You can use it as an example.
+	MatcherRE *regexp.Regexp
+}
+
+// ParseFile parses everything from r using filename to make associations.
+func (p *TestParser) ParseFile(filename string, r io.Reader) (*TestFile, error) {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, fmt.Errorf("read test file: %v", err)
+	}
+	return p.parseFile(filename, data)
+}
+
+var defaultMatcherRE = regexp.MustCompile(`^\s*//([=~]) (.*)`)
---
+++ internal/end2end/parser.go
@@ -0,0 +1,60 @@
+package end2end
+
+import (
+	"fmt"
+	"go/token"
+	"regexp"
+	"strings"
+)
+
+func (p *TestParser) parseFile(filename string, data []byte) (*TestFile, error) {
+	matchers := make(map[int][]*Matcher)
+	var pending []*Matcher
+
+	f := &TestFile{Matchers: matchers, text: string(data)}
+
+	for i, l := range strings.Split(f.text, "\n") {
+		line := i + 1
+		op, text, ok := p.fetchMatcher(l)
+		switch {
+		case ok:
+			m := Matcher{text: text, pos: token.Position{Filename: filename}}
+			switch op {
+			case "~":
+				re, err := regexp.Compile(text)
+				if err != nil {
+					return nil, fmt.Errorf("%s:%d: %v", filename, line, err)
+				}
+				m.re = re
+			case "=":
+				// Do nothing.
+			default:
+				return nil, fmt.Errorf("%s:%d: unknown op %q", filename, line, op)
+			}
+			pending = append(pending, &m)
+
+		case len(pending) != 0:
+			for _, m := range pending {
+				m.pos.Line = line
+			}
+			// Copy all matchers from the pending list to the result.
+			matchers[line] = append([]*Matcher{}, pending...)
+			pending = pending[:0] // Clear pending list
+		}
+	}
+
+	return f, nil
+}
+
+func (p *TestParser) fetchMatcher(l string) (op, text string, ok bool) {
+	re := defaultMatcherRE
+	if p.MatcherRE != nil {
+		re = p.MatcherRE
+	}
+
+	m := re.FindStringSubmatch(l)
+	if len(m) < 2 {
+		return "", "", false
+	}
+	return m[1], m[2], true
+}
